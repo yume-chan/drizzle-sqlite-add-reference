cfc146b2805e4352f0e88d1e482dd82de6231e3e
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2024-07-26 10:52:34 +0800

    add self reference

--- schema.ts
+++ schema.ts
@@ -5,4 +5,5 @@ export const users = sqliteTable('users', {
   id: text('id'),
   textModifiers: text('text_modifiers').notNull().default(sql`CURRENT_TIMESTAMP`),
   intModifiers: integer('int_modifiers', { mode: 'boolean' }).notNull().default(false),
+  parent: text('parent').references((): AnySQLiteColumn => users.id)
 });
